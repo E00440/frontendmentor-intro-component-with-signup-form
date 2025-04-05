1ea199a2689f0efb24fd2d1f14f5102c12159260
Emmanuel Malik <emmanuel00440@gmail.com>
2025-04-05 17:39:21 +0100

    button hover and js inputs selection

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const firstNameInput = document.querySelector('.js-first-name');
+
+const lastNameInput = document.querySelector('.js-last-name');
+
+const emailInput = document.querySelector('.js-email');
+
+const passwordInput = document.querySelector('.js-password');
+
+const buttonElement = document.querySelector('.js-submit-button');
+
+buttonElement.addEventListener('click', () => {
+  
+});
